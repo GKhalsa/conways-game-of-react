e318bdd6eb18f673be9554a358e66b78826ed139
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2017-05-29 11:36:20 -0500

    remove commented out code

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Cell from './Cell'
+import Cell from './Cell';
 
 export default class Board extends Component {
 
--- src/Cell.js
+++ src/Cell.js
@@ -2,16 +2,7 @@ import React,{Component} from 'react'
 
 export default class Cell extends Component {
 
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     alive: false
-  //   };
-  //
-  // }
-
   render(){
-    // console.log("render",this.props.index, this.props.alive)
     let backgroundColor;
     if (this.props.alive){ backgroundColor = "background-color";}
 
